9ec827e9ce28f6428a80b99193cbbb95f7029ca8
Saravana Raja <saravanaraja25@gmail.com>
2022-03-20 00:00:45 +0530

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/todos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todos',
+    component: CategoryComponent,
+    // children: [
+    //   {
+    //     path: '',
+    //     loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule),
+    //   }
+    // ]
+  },
+  {
+    path: 'todos/:id',
+    loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/todos'
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
