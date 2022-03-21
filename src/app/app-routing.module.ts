4b43c42403511f23e364c7b52941d2aa69264816
Saravana Raja <saravanaraja25@gmail.com>
2022-03-21 10:01:10 +0530

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-  },
-  {
     path: 'todos',
     component: CategoryComponent,
   },
